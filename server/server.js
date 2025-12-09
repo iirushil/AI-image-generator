42117878734068df4ece40ad53229babc653ceba
iirushil <rushil4835@gmail.com>
2025-12-09 11:35:16 +0530

    did some work

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import cors from 'cors'
 import 'dotenv/config'
 import connectDB from './config/mongodb.js'
 import userRouter from './routes/userroutes.js'
+import imageRouter from './routes/imageroutes.js'
 
 const port = process.env.PORT || 4000;
 const app = express();
@@ -13,6 +14,7 @@ app.use(cors());
 await connectDB();
 
 app.use('/api/user' , userRouter)
+app.use('/api/user' , imageRouter)
 app.get ('/' , (req ,res) => {
     res.send('API Working')
 });
