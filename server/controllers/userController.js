22b3c7d483da3bc07a37d8750765789f70e3d661
iirushil <rushil4835@gmail.com>
2025-12-11 14:58:53 +0530

    put the feature of razor pay

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,7 +1,10 @@
 import userModel from "../models/usermodel.js"
-import bcrypt from 'bcrypt.js'
+import bcrypt from 'bcrypt'
 import jwt from 'jsonwebtoken'
 import validator from 'validator'
+import Razorpay from 'razorpay'
+import transactionModel from "../models/transactionModel.js"
+
 
 const registerUser = async (req , res) => {
     try{
@@ -72,8 +75,100 @@ const userCredits = async (req , res) => {
          console.log(error)
         res.json({success:false , message:error.message})
     }
-
 }
 
-export {registerUser , loginUser , userCredits}
+const razorpayInstance = new razorpay({
+    key_id: process.env.RAZORPAY_KEY_ID,
+    key_secret:process.env.RAZORPAY_KEY_SECRET
+})
+
+const paymentRazorpay = async (req,res) => {
+    try {
+        const {userId , planId} = req.body
+        const userDta = await userModel.findById(userId)
+
+        if(!userDta || !planId){
+            return res.json({success:false , message:'Missing Details'})
+        }
+         let credits, plan, amount, date
+
+        switch (planId) {
+            case 'Basic':
+                plan = 'Basic'
+                credits = 25
+                amount = 10
+                break;
+            case 'Advanced':
+                plan = 'Advanced'
+                credits = 70
+                amount = 30
+                break;
+            case 'Premier':
+                plan = 'Premier'
+                credits = 150
+                amount = 50
+                break;
+        
+            default:
+                return res.json({success: false, message: 'plan not found'});
+        }
+
+        date = Date.now();
+
+        const transactionData ={
+            userId, plan, amount, credits, date
+        }
+
+        const newTransaction = await transactionModel.create(transactionData)
+
+        const options = {
+            amount:amount * 100,
+            currency: process.env.CURRENCY,
+            receipt: newTransaction._id,
+        }
+
+        await razorpayInstance.orders.create(options, (error, order)=>{
+            if (error) {
+                console.log(error);
+                return res.json({success:false, message:error})
+            }
+            res.json({success: true, order})
+        })
+
+
+    } catch (error) {
+        console.log(error)
+        res.json({ success: false, message: error.message })
+    }
+}
+
+// API to verify payment of razorpay
+const verifyRazorpay = async (req, res) => {
+    try {
+        const { razorpay_order_id } = req.body
+        const orderInfo = await razorpayInstance.orders.fetch(razorpay_order_id)
+
+        if (orderInfo.status === 'paid') {
+            const transactionData = await transactionModel.findById(orderInfo.receipt)
+            if (transactionData.payment) {
+                return res.json({success: false, message: 'Payment Failed'})
+            }
+
+            const userData = await userModel.findById(transactionData.userId)
+
+            const creditBalance = userData.creditBalance + transactionData.credits
+            await userModel.findByIdAndUpdate(userData._id, {creditBalance})
+            await transactionModel.findByIdAndUpdate(transactionData._id, {payment:true})
+            res.json({ success: true, message: "Credits Added" })
+        }
+        else {
+            res.json({ success: false, message: 'Payment Failed' })
+        }
+    } catch(error) {
+        console.log(error)
+        res.json({ success: false, message: error.message })
+    }
+}
+
+export {registerUser , loginUser , userCredits , paymentRazorpay , verifyRazorpay}
 
--- server/routes/userroutes.js
+++ server/routes/userroutes.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { registerUser , loginUser , userCredits } from '../controllers/userController.js'
+import { registerUser , loginUser , userCredits, paymentRazorpay, verifyRazorpay } from '../controllers/userController.js'
 import authUser from '../middleware/auth.js'
 
 const userRouter = express.Router()
@@ -7,5 +7,7 @@ const userRouter = express.Router()
 userRouter.post('/register' , registerUser)
 userRouter.post('/login' , loginUser)
 userRouter.get('/credits' , authUser , userCredits)
+userRouter.post('/pay-razor' , authUser , paymentRazorpay)
+userRouter.post('/verify-razor' , verifyRazorpay )
 
 export default userRouter
